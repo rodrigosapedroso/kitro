be744a0569c35c39ad134ad7099ad30dddb3bc19
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-02-06 19:59:08 +0000

    defining schemas for data output

---
+++ backend/app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class ProductOut(BaseModel):
+    name: str
+    stock_quantity: int
+    total_sold: int
+    price: float
+
+    class Config:
+        orm_mode = True
+
+class MetricsOut(BaseModel):
+    total_stock: int
+    total_sold: int
+    total_gains_after_taxes: float
