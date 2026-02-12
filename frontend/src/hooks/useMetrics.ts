8d93b6b38cc4eac35a4332b082796c2e92c687c5
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2026-02-12 00:19:51 +0000

    added custom hooks for products and metrics fetching

---
+++ frontend/src/hooks/useMetrics.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import type { Metrics } from "../types";
+import { getMetrics } from "../services/api";
+
+export default function useMetrics() {
+
+  const [metrics, setMetrics] = useState<Metrics | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  
+  useEffect(() => {
+    async function fetchMetrics() {
+      const timer = setTimeout(() => setLoading(true), 50) //after 50ms, shoot setLoading(true)
+      try {
+        const data = await getMetrics();
+        setMetrics(data);
+      } catch {
+        setError("Failed to fetch metrics.");
+      } finally {
+        clearTimeout(timer) //if fetch under 50ms, cancel timer
+        setLoading(false);
+      }
+    }
+    fetchMetrics();
+  }, []);
+
+  return { metrics, loading, error };
+}
---
+++ frontend/src/hooks/useProducts.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../services/api"
+import type { Products } from "../types";
+
+export default function useProducts() {
+  
+  const [products, setProducts] = useState<Products[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const timer = setTimeout(() => setLoading(true), 50) //after 50ms, shoot setLoading(true)
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (err) {
+        setError("Failed to fetch products.");
+      } finally {
+        clearTimeout(timer) //if fetch under 50ms, cancel timer
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+}
